c30614ed3789e996f02ca52119ee2a88ae2491b5
jeyaram1023 <jeyaram.reddy.ece@gmail.com>
2025-06-14 14:45:12 +0530

    Update js_superbase.js
    Add API key

--- js_superbase.js
+++ js_superbase.js
@@ -1,6 +1,6 @@
 // js_superbase.js
-const SUPABASE_URL = 'YOUR_SUPABASE_URL'; // Replace with your Supabase Project URL
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY'; // Replace with your Supabase Public Anon Key
+const SUPABASE_URL = 'https://rnjvqxdrvplgilqzwnpl.supabase.co'; // Replace with your Supabase Project URL
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJuanZxeGRydnBsZ2lscXp3bnBsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg5NDU4NjYsImV4cCI6MjA2NDUyMTg2Nn0.IOAxp8ULZgccX8hKtlDQzwdrW7xp1CcXVSdJ59UEruA'; // Replace with your Supabase Public Anon Key
 
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
     console.error("Supabase URL or Anon Key is missing. Please check your js_superbase.js file.");
